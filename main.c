8fc7306b9cc9619220bab123ed7eb44927a7aac4
Russell Smith <qqrsmith@gmail.com>
2013-03-11 12:49:16 -0400

    compare sort-under-test to stdlib qsort and report OK/FAIL

--- main.c
+++ main.c
@@ -8,6 +8,8 @@
 int main(int argc, char *argv[])
 {
     int *list;
+    int *list_sort;
+    int *list_stdsort;
     uint32_t list_size;
 
     if ( argc <= 0 ) {
@@ -16,16 +18,36 @@ int main(int argc, char *argv[])
         // run tests
     } else {
         list_size = argc - 1;
+
         list = malloc( list_size * sizeof(int) );
-        if (!list) { 
+        list_sort = malloc( list_size * sizeof(int) );
+        list_stdsort = malloc( list_size * sizeof(int) );
+        if (!list || !list_sort || !list_stdsort) { 
             exit(EXIT_FAILURE);
         }
+
         for (int i = 0; i < list_size; i++) {
             list[i] = atoi(argv[i+1]);
         }
+        memcpy(list_sort, list, list_size*sizeof(int));
+        memcpy(list_stdsort, list, list_size*sizeof(int));
+
+        sort_under_test(list_sort, list_size);
+        stdsort(list_stdsort, list_size);
+
+        dbgprintf("\n\n");
+        dbgprintf("unsorted:        ");
         dbgprintl(list, list_size);
-        stdsort(list, list_size);
-        dbgprintl(list, list_size);
+        dbgprintf("stdlib qsort:    ");
+        dbgprintl(list_stdsort, list_size);
+        dbgprintf("sort-under-test: ");
+        dbgprintl(list_sort, list_size);
+
+        if ( memcmp(list_sort, list_stdsort, list_size*sizeof(int)) == 0 ) {
+            printf("sort OK\n");
+        } else {
+            printf("sort FAILED\n");
+        }
     }
 
     exit(EXIT_SUCCESS);
--- sort.h
+++ sort.h
@@ -1,6 +1,10 @@
 #ifndef SORT_H
 #define SORT_H
 
+#ifndef sort_under_test
+#define sort_under_test quicksort
+#endif
+
 void quicksort( int *base, size_t size );
 void inssort( int *base, size_t size );
 void inssort2( int *base, size_t size );
